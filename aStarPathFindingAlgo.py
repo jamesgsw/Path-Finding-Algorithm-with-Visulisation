ab0708799b80110b19595ee2d247b755955c76df
James Gan <james_gan@mymail.sutd.edu.sg>
2020-07-23 13:55:53 +0800

    Added A* Algo Source Code

--- aStarPathFindingAlgo.py
+++ aStarPathFindingAlgo.py
@@ -5,7 +5,6 @@ from queue import PriorityQueue
 WIDTH = 800
 # Setting the display as a square
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-
 pygame.display.set_caption("A* Path Finding Algorithm")
 
 #Setting RGB values for the Colors
@@ -105,7 +104,7 @@ class Spot:
             self.neighbors.append(grid[self.row][self.col - 1])
 
         if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): #RIGHT
-            self.neighbors.append(grid[self.row + 1][self.col])
+            self.neighbors.append(grid[self.row][self.col + 1])
 
     def __lt__(self, other):
         """"This method compares 2 block and return a boolean values"""
@@ -117,6 +116,57 @@ f(n) = g(n) + h(n)
 There are 2 components to the A* Algorithm which are the (1) Cost Function, g(n) and (2) Heuristic Function, h(n).
 """
 
+# (1) Cost Function, g(n)
+def algorithm(draw, grid, start, end):
+    """
+    Inputs: draw method that creates each block, 2D array of every block object
+    Outputs:
+    Methods: Crreating the search algorithm where we perform heursitic on the best path by searching the current node and its neighbor
+    """
+    count = 0
+    open_set = PriorityQueue() #Package we installed at the start that allows us to keep track the nodes in the open set, allows us to get the mimimum count from the queue when we use the .get() method
+    open_set.put((0, count, start)) #(Add start node with original F score of 0, when the node was added, starting node)
+    came_from = {} #Keeping track of our path, where each node links to the other, so when the best path is found, we can trace back the shortest path
+
+    #Setting the initial f_score, g_score and h_score when we have defined the starting and ending node
+    g_score = {spot: float("inf") for row in grid for spot in row} #Set each block to have a value of infinity with a nested for loop
+    g_score[start] = 0
+    f_score = {spot: float("inf") for row in grid for spot in row} #Set each block to have a value of infinity with a nested for loop
+    f_score[start] = h(start.get_pos(), end.get_pos()) #Since the g_score is zero, our f_score is only the heuristic of the distance from start to end node
+
+    open_set_hash = {start}
+    while not open_set.empty(): #Run until the open set has been exhausted
+
+        for event in pygame.event.get(): #Handle when the user quits the game
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2] #get current node
+        open_set_hash.remove(current) #Since we are not considering the current node, we can remove it from the open set
+
+        if current == end: #Shortest Path found, then we can draw the path
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True
+
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1 #The neighbor is 1 value past the curent node therefore it's + 1
+            if temp_g_score < g_score[neighbor]: #If better g_score found, them update path
+                came_from[neighbor] = current #Update the came from dict to where this neighbor node came from, which is from our current node that has the shortest path
+                g_score[neighbor] = temp_g_score #Since this is our new shortest path in the if statement above, therefore we can reassign the g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor)) #Adding the neighbor node to the open set in the open_set queue
+                    open_set_hash.add(neighbor) #The neighbor is part of the open set
+                    neighbor.make_open() #Since now the neighbor is part of the open set, we need to update the color
+
+        draw()
+        if current != start:
+            current.make_closed()
+
+    return False
+
 # (2) Heuristic Function, h(n)
 def h(p1, p2):
     """
@@ -127,6 +177,20 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1-x2) + abs(y1-y2)
+
+"""
+Draw the best path
+"""
+def reconstruct_path(came_from, current, draw):
+    """
+    Inputs: List of optimal nodes, starting node, draw object
+    Outputs: Returns nothing
+    Method: Draw most optimal path
+    """
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
 
 """
 Creating a container which holds the status/color of each block
@@ -213,8 +277,7 @@ def main(win, width):
         for event in pygame.event.get(): #This method gets all the events that pygame records
             if event.type == pygame.QUIT: # When we quit the program
                 run = False
-            if started:
-                continue
+
             if pygame.mouse.get_pressed()[0]: #Index 0 is the left mouse button
                 pos = pygame.mouse.get_pos()
                 row, col = get_clicked_pos(pos, ROWS, width) #Using our defined get_clicked_pos method to get the row and column value
@@ -223,11 +286,9 @@ def main(win, width):
                 if not start and spot != end: #Defining the start node
                     start = spot
                     start.make_start()
-
                 elif not end and spot != start: #Defning the end node
                     end = spot
                     end.make_end()
-
                 elif spot != end and spot != start:
                     spot.make_barrier()
 
@@ -243,12 +304,16 @@ def main(win, width):
                     end = None
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for spot in row:
-                            spot.update_neighbors()
+                            spot.update_neighbors(grid)
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
-                    algorithm(Lamda: draw(win, grid, ROWS, width), grid, start, end)
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
 
     pygame.quit()
 
